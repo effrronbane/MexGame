a2b42395b13bb14d6e226245dc2e36258a7399ab
effrronbane <ggutie31@ucsc.edu>
2026-03-09 12:47:26 -0700

    added font and img, all the scenes ill need, worked on start scene, the neurtal scene, and beach scene a bit

---
+++ src/main.js
@@ -0,0 +1,15 @@
+//Gustavo
+
+let config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: [Start, Neutral, Beach] // add four scenes
+}
+
+
+const game = new Phaser.Game(config)
+const {width, height} = game.config
+const centerX = game.config.width / 2
+const centerY = game.config.height / 2
+let cursors = null
